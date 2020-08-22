ad1c7263a825d09c38ff714e9bfc326be32b9467
Afolabi Olayinka <folabiolayinka@gmail.com>
2020-08-22 06:10:25 +0100

    Update DatabaseSeeder.php
    Creating administrator

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +13,18 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        
+        DB::table('users')->insert([
+            [
+                'display_name' => 'Administrator',
+                'first_name' => 'Administrator',
+                'mobile_number' => '08030000000',
+                'email' => 'admin@gotrade.com',                
+                'gender' => 'male',
+                'password' => Hash::make('Admin$123'),
+                'is_active' => 1,
+                'role' => 'admin',
+            ],
+        ]);
     }
 }
