8ae93a44e9300be5cae1b60917b280757ecfa4df
Afolabi Olayinka <folabiolayinka@gmail.com>
2020-08-25 19:08:52 +0100

    Introducing the API default response

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});*/
+
+Route::prefix('/v1')->namespace('API')
+    ->group(function () {
+      Route::get('/', [
+        'uses' => 'DefaultController@index'
+    ]);
 });
