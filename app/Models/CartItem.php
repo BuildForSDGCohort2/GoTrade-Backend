1c3b597086bf942e4340952bc6ba1d0e5499d73a
Afolabi Olayinka <folabiolayinka@gmail.com>
2020-09-10 12:22:24 +0100

    Update and rename CartItems.php to CartItem.php

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class CartItems extends Model
+class CartItem extends Model
 {
     /**
      * The table associated with the model.
